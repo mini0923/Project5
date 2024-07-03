8e07400db5f55835c22cf1fab59a163490b778a8
seongmin <sungmin990923@naver.com>
2024-07-03 17:10:17 +0900

    주석 추가

--- src/method/quiz/Quiz7.java
+++ src/method/quiz/Quiz7.java
@@ -9,9 +9,9 @@ public class Quiz7 {
 		int result4 =0;
 		System.out.print(n1);
 		
-		result1 = n1  / 100 ;
-		result2 = n1 % 100 / 10;
-		result3 = n1 % 10;
+		result1 = n1  / 100 ;			// 100 자리
+		result2 = n1 % 100 / 10; // 10자리
+		result3 = n1 % 10; 			// 1자리
 		
 		result4 = (result1+ result2 + result3);  
 		System.out.println(":" + result4);
